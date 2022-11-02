bcbadc9742cfe6c561791b0ce83ba8d5b81e46ca
Nicolas <nicomartin854@gmail.com>
2022-11-02 20:54:36 -0300

    asd

--- src/pages/ClienteNuevo.jsx
+++ src/pages/ClienteNuevo.jsx
@@ -1,5 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import Formulario from "../components/Formulario";
+
 const ClienteNuevo = () => {
-  return <h1>Agregando cliente</h1>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo cliente</h1>
+      <p className="mt-3"> Llena los datos para registrar un nuevo cliente</p>
+
+      <div className="flex justify-end">
+        <button
+          onClick={() => navigate(-1)}
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto my-2 mb-20 px-5 py-10">
+        <form>
+          <Formulario />
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default ClienteNuevo;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
 
 export function loader() {
   const clientes = [
@@ -50,7 +51,20 @@ const Index = () => {
       <p className="mt-3"> Administra tus clientes</p>
 
       {clientes.length ? (
-        clientes.map((cliente) => cliente.nombre)
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th>Cliente</th>
+              <th>Contacto</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <Cliente cliente={cliente} key={cliente.id} />
+            ))}
+          </tbody>
+        </table>
       ) : (
         <p className="text-center mt-10">No hay clientes aun</p>
       )}
